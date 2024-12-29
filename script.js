37654e273544e70badbc1699415176b4ff51ad92
Nov games <abdoelnoby065@gmail.com>
2024-12-29 21:18:41 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-    let nam = document.getElementById('name').value;
-    let pass = document.getElementById('password').value;
-function clk(){
-    localStorage.setItem("name" , nam);
-    localStorage.setItem("password" , pass);
-}
